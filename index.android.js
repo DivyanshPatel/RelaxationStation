058dcb9ca3a2df5e702d2ebf538f1cbf9b26174d
DivyanshPatel <nealbay@gmail.com>
2017-08-13 14:41:03 +0530

    Added Zen image button

--- index.android.js
+++ index.android.js
@@ -5,15 +5,23 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
+
+const zenImage = require('./assets/zen.png');
 
 export default class RelaxationStation extends Component {
   render() {
     return (
       <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={ () => { alert('I am pressed') }}>
 
-      <Text style={styles.readyText}> I am ready to relax </Text>
+          <Image style={styles.imageButton} source={zenImage}/>
+
+      </TouchableOpacity>
+      <Text style={styles.readyText}> I am ready to relax... </Text>
         
       </View>
     );
@@ -32,6 +40,21 @@ const styles = StyleSheet.create({
     fontStyle: 'italic',
     color: '#ffffff'
   },
+
+  button: {
+    backgroundColor: '#859a9b',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: {width: 0 , height: 5 },
+    shadowRadius: 10 ,
+    shadowOpacity: 0.35,
+  },
+  imageButton: {
+    width: 100,
+    height: 100,
+  }
 });
 
 AppRegistry.registerComponent('RelaxationStation', () => RelaxationStation);
